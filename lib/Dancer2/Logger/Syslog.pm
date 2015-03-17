2fa0c353a44543ce7ff581a1723fc292b8029231
Andrew Beverley <a.beverley@ctrlo.com>
2015-03-17 09:43:13 +0000

    Bump version to 0.2

--- lib/Dancer2/Logger/Syslog.pm
+++ lib/Dancer2/Logger/Syslog.pm
@@ -1,6 +1,6 @@
 package Dancer2::Logger::Syslog;
 # ABSTRACT: Dancer2 logger engine for Sys::Syslog
-$Dancer2::Logger::Syslog::VERSION = '0.1';
+$Dancer2::Logger::Syslog::VERSION = '0.2';
 
 use Moo;
 use File::Basename 'basename';
@@ -84,7 +84,7 @@ Dancer2::Logger::Syslog - Dancer2 logger engine for Sys::Syslog
 
 =head1 VERSION
 
-version 0.1
+version 0.2
 
 =head1 DESCRIPTION
 
